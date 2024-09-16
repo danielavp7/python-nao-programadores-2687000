ded19d579efe3b32a05fb41aa1fb847c26055385
danielavp7 <daniela.pedrosov7@gmail.com>
2024-09-16 23:26:33 +0000

    resolução exercicio 4

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -1,6 +1,23 @@
 ano_nascimento = 1989
 ano_formatura = 2010
 
+print (bool (1))
+print (bool (-1))
+print (bool ("Daniela"))
+print (bool ())
+# none representa ausência de valor
+
+print ("Sara"+" "+"estuda"+" "+"python.")
+print ("Sara "*4)
+print (10/3)
+print (10//3)
+A=3
+B=10
+print (A+B)
+print (B//A)
+
+
+
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
 
 
