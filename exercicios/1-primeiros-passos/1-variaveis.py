9146ada2905e2d208dc3e97040a32951a61ab395
danielavp7 <daniela.pedrosov7@gmail.com>
2024-09-16 12:58:10 +0000

    resolução exercicio 1

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,15 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+name= 'daniela'
+age= "33"
+hobbies='''ler
+música
+passear'''
 # Declare variáveis de cada tipo numérico
-
-
+vencimento= "1002"
+tempo = '0.02'
+complexo= "7uji"
 # Declare uma variável do tipo booleano
+aprender="true"
+procrastinar="false"
